b7cc7eb2da4451b93f6277d0376ae2528c2d978c
caperaven@icloud.com <caperaven@icloud.com>
2017-02-03 11:30:49 +0200

    menu

--- src/components/menu/menu-items.js
+++ src/components/menu/menu-items.js
@@ -1,457 +1,607 @@
 export const menuItems = [
     {
+        id: 1,
         text: "Asset Register"
     },
     {
+        id: 2,
         text: "Assets"
     },
     {
+        id: 1,
         text: "Asset Types"
     },
     {
+        id: 1,
         text: "Asset Configuration"
     },
     {
+        id: 1,
         text: "Asset Importances"
     },
     {
+        id: 1,
         text: "Locations"
     },
     {
+        id: 1,
         text: "Acquisition Types"
     },
     {
+        id: 1,
         text: "Qcquisition Statuses"
     },
     {
+        id: 1,
         text: "Options and Rules"
     },
     {
+        id: 1,
         text: "Option Categories"
     },
     {
+        id: 1,
         text: "Options"
     },
     {
+        id: 1,
         text: "Rules"
     },
     {
+        id: 1,
         text: "Asset Care Plans"
     },
     {
+        id: 1,
         text: "Criticality Configuration"
     },
     {
+        id: 1,
         text: "Probabilities"
     },
     {
+        id: 1,
         text: "Consequence Categories"
     },
     {
+        id: 1,
         text: "Consequences"
     },
     {
+        id: 1,
         text: "Criticality Models"
     },
     {
+        id: 1,
         text: "Scheduling Configuration"
     },
     {
+        id: 1,
         text: "Scenarios"
     },
     {
+        id: 1,
         text: "Calendars"
     },
     {
+        id: 1,
         text: "Meters"
     },
     {
+        id: 1,
         text: "Meter Fule Types"
     },
     {
+        id: 1,
         text: "Meter Readings"
     },
     {
+        id: 1,
         text: "Events"
     },
     {
+        id: 1,
         text: "Task Interval Types"
     },
     {
+        id: 1,
         text: "Tasks Configuration"
     },
     {
+        id: 1,
         text: "Standard Tasks"
     },
     {
+        id: 1,
         text: "Types of Work"
     },
     {
+        id: 1,
         text: "Task Importances"
     },
     {
+        id: 1,
         text: "Task Roll Up Points"
     },
     {
+        id: 1,
         text: "Special Resources"
     },
     {
+        id: 1,
         text: "Failture ANalysis"
     },
     {
+        id: 1,
         text: "Failures"
     },
     {
+        id: 1,
         text: "Root Causes"
     },
     {
+        id: 1,
         text: "Repair Types"
     },
     {
+        id: 1,
         text: "Maintenance Manager"
     },
     {
+        id: 1,
         text: "Requests"
     },
     {
+        id: 3,
         text: "Work Orders"
     },
     {
+        id: 1,
         text: "Work Order Costing"
     },
     {
+        id: 1,
         text: "Work Order Queues"
     },
     {
+        id: 1,
         text: "Asset Work Orders Lookup"
     },
     {
+        id: 1,
         text: "Work Order Configuration"
     },
     {
+        id: 1,
         text: "Work Order Imporances"
     },
     {
+        id: 1,
         text: "Work Order Statuses"
     },
     {
+        id: 1,
         text: "Availability Loss Categories"
     },
     {
+        id: 1,
         text: "Availability Loss Types"
     },
     {
+        id: 1,
         text: "Availability Loss Reasons"
     },
     {
+        id: 4,
         text: "Planning and Scheduling"
     },
     {
+        id: 1,
         text: "Proposed Work Orders"
     },
     {
+        id: 1,
         text: "Proposed Duplicate Tasks"
     },
     {
+        id: 1,
         text: "Planning Work Orders"
     },
     {
+        id: 1,
         text: "Planning Events"
     },
     {
+        id: 1,
         text: "Monitoring"
     },
     {
+        id: 1,
         text: "Monitoring Point Types"
     },
     {
+        id: 1,
         text: "Monitoring Points"
     },
     {
+        id: 1,
         text: "Monitoring Point Readings"
     },
     {
+        id: 1,
         text: "Alarms"
     },
     {
+        id: 1,
         text: "Alarm Types"
     },
     {
+        id: 1,
         text: "Service Management"
     },
     {
+        id: 1,
         text: "Service Level Agreement"
     },
     {
+        id: 1,
         text: "Material Manager"
     },
     {
+        id: 1,
         text: "Requisitioning"
     },
     {
+        id: 1,
         text: "Requisitions"
     },
     {
+        id: 1,
         text: "Requisition Queues"
     },
     {
+        id: 1,
         text: "Requisition Statuses"
     },
     {
+        id: 1,
         text: "Issues and Returns"
     },
     {
+        id: 1,
         text: "Purchase Orders"
     },
     {
+        id: 1,
         text: "Purchase Order Queues"
     },
     {
+        id: 1,
         text: "Proposed Purchase Orders"
     },
     {
+        id: 1,
         text: "Purchase Order Statuses"
     },
     {
+        id: 1,
         text: "Receipts"
     },
     {
+        id: 1,
         text: "Credit Notes"
     },
     {
+        id: 1,
         text: "Stock Items"
     },
     {
+        id: 1,
         text: "Material Master"
     },
     {
+        id: 1,
         text: "Warehouses"
     },
     {
+        id: 1,
         text: "Commodities"
     },
     {
+        id: 1,
         text: "Stock Categories"
     },
     {
+        id: 1,
         text: "Suppliers"
     },
     {
+        id: 1,
         text: "Stock Analysis"
     },
     {
+        id: 1,
         text: "Material Transaction List"
     },
     {
+        id: 1,
         text: "Stock Financials"
     },
     {
+        id: 1,
         text: "Stock Changes"
     },
     {
+        id: 1,
         text: "Stock Revaluation"
     },
     {
+        id: 1,
         text: "Stock Take"
     },
     {
+        id: 1,
         text: "Stock Transfer"
     },
     {
+        id: 1,
         text: "Performance Manager"
     },
     {
+        id: 1,
         text: "Log Sheets"
     },
     {
+        id: 1,
         text: "Loss Configuration"
     },
     {
+        id: 1,
         text: "Availability Loss Categories"
     },
     {
+        id: 1,
         text: "Availability Loss Types"
     },
     {
+        id: 1,
         text: "Availability Loss Reasons"
     },
     {
+        id: 1,
         text: "Performance Loss Reasons"
     },
     {
+        id: 1,
         text: "Quality Loss Reasons"
     },
     {
+        id: 1,
         text: "Production Configuration"
     },
     {
+        id: 1,
         text: "Products"
     },
     {
+        id: 1,
         text: "Production Batches"
     },
     {
+        id: 1,
         text: "Shift Configuration"
     },
     {
+        id: 1,
         text: "Shifts"
     },
     {
+        id: 1,
         text: "Shift Sets"
     },
     {
+        id: 1,
         text: "User Configuration"
     },
     {
+        id: 1,
         text: "User Lists"
     },
     {
+        id: 1,
         text: "Staff"
     },
     {
+        id: 1,
         text: "Trades"
     },
     {
+        id: 1,
         text: "Sections"
     },
     {
+        id: 1,
         text: "Staff Competencies"
     },
     {
+        id: 1,
         text: "Staff Members"
     },
     {
+        id: 1,
         text: "Financials"
     },
     {
+        id: 1,
         text: "Budget Tempaltes"
     },
     {
+        id: 1,
         text: "Budgets"
     },
     {
+        id: 1,
         text: "Financial Years"
     },
     {
+        id: 1,
         text: "Site Financial Years"
     },
     {
+        id: 1,
         text: "Cost Centre Types"
     },
     {
+        id: 1,
         text: "Cost Centres"
     },
     {
+        id: 1,
         text: "Cost Elements"
     },
     {
+        id: 1,
         text: "General Ledger"
     },
     {
+        id: 1,
         text: "Currencies"
     },
     {
+        id: 1,
         text: "Currency Rates"
     },
     {
+        id: 1,
         text: "Analysis and Monitoring"
     },
     {
+        id: 1,
         text: "Reports"
     },
     {
+        id: 1,
         text: "Background Tasks"
     },
     {
+        id: 1,
         text: "Scheduled Background Tasks"
     },
     {
+        id: 1,
         text: "Shared Configuration"
     },
     {
+        id: 1,
         text: "Notification Manager"
     },
     {
+        id: 1,
         text: "Service Level Triggers"
     },
     {
+        id: 1,
         text: "Service Level Evaluation Triggers"
     },
     {
+        id: 1,
         text: "Work Order Status Change Triggers"
     },
     {
+        id: 1,
         text: "Scheduled Triggers"
     },
     {
+        id: 1,
         text: "Monitoring Alarm Triggers"
     },
     {
+        id: 1,
         text: "Notification Template"
     },
     {
+        id: 1,
         text: "Notification Audit"
     },
     {
+        id: 1,
         text: "Communication Providers"
     },
     {
+        id: 1,
         text: "Documents"
     },
     {
+        id: 1,
         text: "Contracts"
     },
     {
+        id: 1,
         text: "Attributes"
     },
     {
+        id: 1,
         text: "Standard Languages"
     },
     {
+        id: 1,
         text: "Standard Phrases"
     },
     {
+        id: 1,
         text: "Classifications"
     },
     {
+        id: 1,
         text: "User Defined Fields"
     },
     {
+        id: 1,
         text: "Units of Measurement"
     },
     {
+        id: 1,
         text: "Roles"
     },
     {
+        id: 1,
         text: "Users"
     },
     {
+        id: 1,
         text: "Site Types"
     },
     {
+        id: 1,
         text: "Sites"
     },
     {
+        id: 1,
         text: "Search Index Definitions"
     },
     {
+        id: 1,
         text: "System Options"
     },
     {
+        id: 1,
         text: "Business Intelligences"
     }
 ];
 
 export const quickItems = [
     {
+        id: 3,
         text: "Work Orders",
         Icon: "workorder.svg"
     },
     {
+        id: 2,
         text: "Assets",
         Icon: "asset.svg"
     },
     {
+        id: 4,
         text: "Planning and Scheduling",
         Icon: "planning.svg"
     }
--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -31,6 +31,7 @@ export class Menu {
     }
 
     menuSelected(event) {
-        alert(event);
+        const id = event.target.dataset.id;
+        console.log(id);
     }
 }
